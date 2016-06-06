3d6e7bef6dcc475f443a40b22b577528db5dbda0
Maurizio Carboni <maury91@gmail.com>
2016-06-06 19:35:28 +0100

    First Commit

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ProductList from './components/productList';
+
+ReactDOM.render(
+    <ProductList />,
+    document.getElementById('content')
+);
+
---
+++ src/components/productList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class ProductList extends React.Component {
+
+    render() {
+        return (
+            <div className='ui items'>
+                Hello, friend! I am a basic React component.
+            </div>
+        );
+    }
+    
+}
